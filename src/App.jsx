17b631f0dbc9d6e75407dca5e3053eba368eca89
ipekcan <ipekcan@hotmail.com>
2025-04-02 21:05:37 +0300

    hw finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import ContactList from "./features/contacts/ContactsList";
+import AddContactFrom from "./features/contacts/AddContactFrom";
+import SearchBox from "./features/contacts/SearchContact";
+import { useSelector } from "react-redux";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const filtered = useSelector(state => state.filter);
+  const contacts = useSelector(state => state.contacts);
+
+  const filterContact = () => {
+    const filteredContacts = contacts.items.filter(contact =>
+      contact.name.includes(filtered)
+    );
+    return filteredContacts;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <>
+    <AddContactFrom/>
+    <SearchBox/>
+    <ContactList />
+  </>
+  );
 }
 
-export default App
+export default App;
